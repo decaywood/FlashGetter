504b492d5898ad5f7e8f56e4e7e1aa1a3ea6c530
decaywood <decaywood@qq.com>
2015-02-11 21:27:56 +0800

    improve the sizeUnit accuracy

--- src/main/java/flashGetter/util/ParameterUnitUtil.java
+++ src/main/java/flashGetter/util/ParameterUnitUtil.java
@@ -1,5 +1,7 @@
 package flashGetter.util;
 
+import java.text.DecimalFormat;
+
 /**
  * @author decaywood
  * 
@@ -7,14 +9,16 @@ package flashGetter.util;
  * 
  */
 public class ParameterUnitUtil {
+    
+    private static DecimalFormat decimalFormat = new DecimalFormat("##.##");
 
     public static String getFileSize(long fileSize){
         
         if(fileSize < 1000L) return fileSize + " B";
-        if(fileSize < 1000000L) return fileSize / 1000L + " KB";
-        if(fileSize < 1000000000L) return fileSize / 1000000L + " MB";
-        if(fileSize < 100000000000L) return fileSize / 1000000000L + "GB";
-        if(fileSize < 100000000000000L) return fileSize / 1000000000000L + "TB";
+        if(fileSize < 1000000L) return decimalFormat.format(fileSize / 1000D) + " KB";
+        if(fileSize < 1000000000L) return decimalFormat.format(fileSize / 1000000D) + " MB";
+        if(fileSize < 100000000000L) return decimalFormat.format(fileSize / 1000000000D) + "GB";
+        if(fileSize < 100000000000000L) return decimalFormat.format(fileSize / 1000000000000D) + "TB";
         
         return "Size Error!";
         
